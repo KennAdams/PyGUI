5efdfd36d15e4aed74b787f4976a63c15af90497
KennAdams <kenn.adams200@gmail.com>
2023-06-24 19:02:13 +0300

    pack system

--- ToDo.py
+++ ToDo.py
@@ -2,28 +2,26 @@ import tkinter as tk
 window = tk.Tk()
 window.title("To Do")
 window.geometry("300x500")
-label=tk.Label(window,text="TASKS FOR TODAY",font=("Arial",20)).grid(column=0,columnspan=5,row=0)
+titleFrame=tk.Frame(window).pack(side="top")
+label=tk.Label(titleFrame,text="TASKS FOR TODAY",font=("Arial",20)).pack(side="top",expand=True,fill='x')
 
 #Tasks GUI
+tasksFrame=tk.Frame(window).pack()
 taskList=[]
-gridmaxrow=tk.IntVar(value=2)
 def addtask():
     text = entryTask.get()
-    button=tk.Button(window,text=text)
+    button=tk.Button(tasksFrame,text=text)
     taskList.append(button)
-    gridmaxrow.set(gridmaxrow.get()+1)
     for i in taskList:
-        rw=taskList.index(i)
-        i.grid(column=0,row=rw+1)
-    entryTask.grid(column=0,columnspan=4,row=gridmaxrow.get()-1)
-    addbutton.grid(column=4,row=gridmaxrow.get()-1)
+        i.pack()
 
 
 #Adding GUI
-entryTask=tk.Entry(window)
-addbutton=tk.Button(window,text="add",command=addtask)
-entryTask.grid(column=0,columnspan=4,row=gridmaxrow.get()-1)
-addbutton.grid(column=4,row=gridmaxrow.get()-1)
+addFrame=tk.Frame(window).pack(side="bottom",fill='x',expand=True)
+entryTask=tk.Entry(addFrame)
+addbutton=tk.Button(addFrame,text="add",command=addtask)
+entryTask.pack(side="left",fill='x',expand=True)
+addbutton.pack(side="left",fill='x',expand=True)
 
 
 window.mainloop()
